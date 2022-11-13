d56d06c46334fbbf64a40b838f95912fea6d9c87
AgustinBaffo <agusbaffo@gmail.com>
2022-11-12 21:29:24 -0300

    create roulette betTypes and its set function

--- include/roulette.hpp
+++ include/roulette.hpp
@@ -2,16 +2,35 @@
 #define ROULETTE_H
 
 #include <iostream>
+#include <map>
+#include <string>
 
 class Roulette
 {
 public:
+
+    enum BetsTypes{
+        RED,
+        BLACK,
+        EVEN,
+        ODD,
+        LOW,
+        HIGH
+    };
+
     Roulette();
     ~Roulette();
 
-    void run();
+    void spin();
     
 private:
+
+    std::map<BetsTypes,bool> betResult;
+    void setBetResult(int number);
+    void resetBetResult();
+    
+    std::string getBetTypeName(BetsTypes);
+
 
 };
 
--- src/roulette.cpp
+++ src/roulette.cpp
@@ -1,8 +1,13 @@
 #include "roulette.hpp"
+#include "player.hpp"
+
+#define ROULETTE_MAX_NUMBER 37
+#define ROULETTE_MINMAX_LIMIT 19
 
 Roulette::Roulette()
 {
-
+    srand((unsigned int)time(NULL));
+    resetBetResult();
 }
 
 Roulette::~Roulette()
@@ -10,7 +15,92 @@ Roulette::~Roulette()
 
 }
 
+void Roulette::spin(){
 
-void Roulette::run(){
-    std::cout << "Running!" << std::endl;
+    int spinResult = std::rand() % ROULETTE_MAX_NUMBER;
+    setBetResult(spinResult);
+
+    // TEST: show result
+    // std::cout<<std::endl<<spinResult<<") ";
+    // for(auto a: betResult){
+    //     std::cout<<getBetTypeName(a.first)<<": "<<a.second<<", ";
+    // }
+    // std::cout<<std::endl;
+
+}
+
+void getName();
+
+void Roulette::setBetResult(int spinResult){
+
+    resetBetResult();
+
+    if(spinResult==0){
+        return;
+    }
+    
+    if(spinResult<19){
+        betResult[LOW] = true;
+    } else { 
+        betResult[HIGH] = true;
+    }
+
+    if(spinResult%2){
+        betResult[ODD] = true;
+
+        if (spinResult <= 10 || (spinResult > 18 && spinResult <= 28)){
+            betResult[RED] = true;
+        } else { 
+            betResult[BLACK] = true;
+        }
+
+    } else {
+        betResult[EVEN] = true;
+
+        if (spinResult <= 10 || (spinResult > 18 && spinResult <= 28)){
+            betResult[BLACK] = true;
+        } else { 
+            betResult[RED] = true;
+        }
+    }
+
+}
+
+void Roulette::resetBetResult(){
+   betResult = {{RED, false}, 
+                {BLACK, false}, 
+                {EVEN, false}, 
+                {ODD, false}, 
+                {LOW, false}, 
+                {HIGH, false}};
+}
+
+std::string Roulette::getBetTypeName(BetsTypes bet){
+    std::string ret;
+    switch (bet)
+    {
+    case RED:
+        ret = "red";
+        break;
+    case BLACK:
+        ret = "black";
+        break;
+    case EVEN:
+        ret = "even";
+        break;
+    case ODD:
+        ret = "odd";
+        break;
+    case LOW:
+        ret = "low";
+        break;
+    case HIGH:
+        ret = "high";
+        break;
+    default:
+        ret = "unknown";
+        break;
+    }
+
+    return ret;
 }
